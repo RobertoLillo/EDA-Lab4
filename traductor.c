4979d8c9528d791166a61c7f005a4e4bf7f25935
Roberto Lillo <robertoxlp@hotmail.com>
2019-01-07 15:09:18 -0300

    Avance

--- traductor.c
+++ traductor.c
@@ -2,6 +2,37 @@
 
 int main()
 {
+    int cantidad = contarPalabras();
+    if (cantidad > 0) // Se ejecuta solo si se pudo leer el archivo o se encontro al menos una palabra
+    {
+        
+    }
+    else // No se pudo leer el archivo
+    {
+        printf("Se encontro un problema, se cerrara el programa\n");
+    }
+}
 
-    return 0;
+int contarPalabras()
+{
+    FILE *archivo = fopen("traductor.in", "r");
+
+    if (archivo)
+    {
+        int cantidad = 0;
+        char linea[100];
+        while (!feof(archivo))
+        {
+            fgets(linea, 100, archivo);
+            cantidad++;
+        }
+        fclose(archivo);
+        return cantidad;
+    }
+    else
+    {
+        printf("No se pudo leer el archivo\n");
+        fclose(archivo);
+        return -1;
+    }
 }
--- traductor.h
+++ traductor.h
@@ -4,7 +4,17 @@
 #include <math.h>
 
 /********** Estructuras **********/
-
+typedef struct Nodo {
+    char *espanol;
+    struct Nodo *padre;
+    struct Nodo *hijoIzq;
+    struct Nodo *hijoDer;
+    char *english;
+    struct Nodo *father;
+    struct Nodo *leftChild;
+    struct Nodo *RightChild;
+} nodo;
 
 /********** Prototipos **********/
+int contarPalabras();
 
